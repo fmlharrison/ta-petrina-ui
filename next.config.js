1f18b6ebd7eb20fb2d844e45d930ba42cba0fe3f
Felix Harrison <fmlharrison@gmail.com>
2022-07-15 09:22:00 +0100

    Initial page using real data

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["tailwindcss.com", "res.cloudinary.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
